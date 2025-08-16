de20a157d3981cc9694bbef49700f01d2386801a
Clicksurfer <amit.chameides@gmail.com>
2025-08-16 22:09:56 +1000

    Added all blog posts

--- src/data/blogPosts.ts
+++ src/data/blogPosts.ts
@@ -13,6 +13,35 @@ import { smallUpdate } from "./posts/small-update";
 import { fillIsOut } from "./posts/fill-is-out";
 import { fillAlpha2IsOutNow } from "./posts/fill-alpha-2-is-out-now";
 import { fillAlphaIsOutNow } from "./posts/fill-alpha-is-out-now";
+import { pieceOfCake } from "./posts/piece-of-cake";
+import { fillingInThePieces } from "./posts/filling-in-the-pieces";
+import { redditMoment } from "./posts/reddit-moment";
+import { vlogginItUp } from "./posts/vloggin-it-up";
+import { turningOverANewLeaf } from "./posts/turning-over-a-new-leaf";
+import { theFinalMeme } from "./posts/the-final-meme";
+import { iHateThis } from "./posts/i-hate-this";
+import { memeMasterARetrospect } from "./posts/meme-master-a-retrospect";
+import { theMemeMaster } from "./posts/the-meme-master";
+import { whosMemeIsThisAnyway } from "./posts/whos-meme-is-this-anyway";
+import { theTrialsAndTribulationsOfOnlineMultiplayer } from "./posts/the-trials-and-tribulations-of-online-multiplayer";
+import { isThatGoingToBeOnTheTest } from "./posts/is-that-going-to-be-on-the-test";
+import { inSpaceNoOneCanHearYourSfx } from "./posts/in-space-no-one-can-hear-your-sfx";
+import { makingItPop } from "./posts/making-it-pop";
+import { backInTheGame } from "./posts/back-in-the-game";
+import { networkingShenanigans } from "./posts/networking-shenanigans";
+import { thatMomentWhenYouDevAGame } from "./posts/that-moment-when-you-dev-a-game";
+import { springCleaning } from "./posts/spring-cleaning";
+import { youreNotSupposedToBeHereIsPublished } from "./posts/youre-not-supposed-to-be-here-is-published";
+import { theVeryTop } from "./posts/the-very-top";
+import { openBetaIsFinallyHere } from "./posts/open-beta-is-finally-here";
+import { waxOnWaxOff } from "./posts/wax-on-wax-off";
+import { strikeAPose } from "./posts/strike-a-pose";
+import { haventIBeenHereBefore } from "./posts/havent-i-been-here-before";
+import { hidingInTheCorner } from "./posts/hiding-in-the-corner";
+import { youreNotSupposedToBeHereBabySteps } from "./posts/youre-not-supposed-to-be-here-baby-steps";
+import { officiallyWorkingOnANewGame } from "./posts/officially-working-on-a-new-game";
+import { vrIsJustAroundTheCorner } from "./posts/vr-is-just-around-the-corner";
+import { backToTheDrawingBoard } from "./posts/back-to-the-drawing-board";
 
 export interface BlogPost {
   id: string;
@@ -28,17 +57,45 @@ export interface BlogPost {
 export const blogPosts: BlogPost[] = [
   myTop10VideoGamesFor2024,
   cabinetComplete,
+  toPlanAMockingbird,
+  theQuestToConnectConsoles,
   aRetrospectOnVideoGameMonetization,
   anotherOneDown,
   whereInTheWorldIsEmmetgames,
-  toPlanAMockingbird,
-  theQuestToConnectConsoles,
   backEndHeaven,
   smallUpdate,
   fillIsOut,
   fillAlpha2IsOutNow,
   fillAlphaIsOutNow,
-  // Additional posts can be imported and added here
+  pieceOfCake,
+  fillingInThePieces,
+  redditMoment,
+  vlogginItUp,
+  turningOverANewLeaf,
+  theFinalMeme,
+  iHateThis,
+  memeMasterARetrospect,
+  theMemeMaster,
+  whosMemeIsThisAnyway,
+  theTrialsAndTribulationsOfOnlineMultiplayer,
+  isThatGoingToBeOnTheTest,
+  inSpaceNoOneCanHearYourSfx,
+  makingItPop,
+  backInTheGame,
+  networkingShenanigans,
+  thatMomentWhenYouDevAGame,
+  springCleaning,
+  youreNotSupposedToBeHereIsPublished,
+  theVeryTop,
+  openBetaIsFinallyHere,
+  waxOnWaxOff,
+  strikeAPose,
+  haventIBeenHereBefore,
+  hidingInTheCorner,
+  youreNotSupposedToBeHereBabySteps,
+  officiallyWorkingOnANewGame,
+  vrIsJustAroundTheCorner,
+  backToTheDrawingBoard
 ];
 
 export const getFeaturedPost = () => blogPosts.find(post => post.featured);
