779eaa0712003ea0d255dc38f2ab4f27461b0a5c
Clicksurfer <amit.chameides@gmail.com>
2025-08-24 18:19:15 +1000

    Finished monetization retrospective

--- src/data/posts/a-retrospect-on-video-game-monetization.ts
+++ src/data/posts/a-retrospect-on-video-game-monetization.ts
@@ -5,86 +5,117 @@ export const aRetrospectOnVideoGameMonetization: BlogPost = {
   id: "a-retrospect-on-video-game-monetization",
   title: "A Retrospect on Video Game Monetization",
   excerpt: "A critical look at how monetization has evolved in the video game industry and its impact on game design and player experience.",
-  content: `# A Retrospect on Video Game Monetization
+  content: `Hi everyone.
+I've taken some time off to rejuvenate and prepare for my next project(s). It's been going well so far, but it's honestly so difficult to stop myself from just jumping into another project. I have heaps of ideas and lots of energy, but I know I need to slow down and be patient, lest I burn myself out.
 
-The video game industry has undergone massive changes in how games are monetized over the past two decades. What started as a simple transaction - pay once, own forever - has evolved into a complex ecosystem of subscription services, microtransactions, battle passes, and live service models.
+I've been thinking a lot about video game monetization recently, and thought to share some insights and retrospective into the history of video game monetization.
+Specifically, I will be examining them from both a practical perspective as well as a moralistic one.
+It practically goes without saying but I am no expert on the topic. In fact - video game monetization is not my specialty in video game development at all. I can (and probably will) get stuff wrong as I write, so do take it with a grain of salt.
 
-## The Golden Age of One-Time Purchases
+So without further ado:
+A brief history of video game monetization.
 
-There was a time when buying a video game was straightforward. You walked into a store, picked up a box, paid your money, and owned that game forever. No additional costs, no ongoing fees, no premium currencies.
+The Arcades and coin-op
+-----------------------
+In the late 70s, video games became first available to the general public via arcade cabinets.
+This was no coincidence - it was a technological and financial restriction. At the time, home consoles (devices capable of running video games in the comfort of your home) were not widespread at all. It was really hard for game developers at the time to make machines that were compact, efficient and cheap enough for consumers to want to buy. Home consoles were exorbitantly expensive, and the games on them were very rudimentary.
+The solution was quite brilliant - take the existing model of coin-operated machines (like those for soda, or carnivals), and have your game set in a public place where anyone could play. The size of the arcade cabinets allowed far more powerful computers to power them, which made the games on them easier to develop and more interesting to play, and this monetization system was both familiar to the consumer and also made it very cheap to play.
 
-This model worked well for both developers and players. Developers had a clear revenue target and players knew exactly what they were getting for their money.
+The monetization model was very simple - people put in a quarter and got to play for a set amount of time, or until they lost. Game developers were able to make a hefty profit by building their game around this concept. The most popular games of the era - _Space Invaders_**,** _Pacman_ and many others were easy to learn and hard to master. Allowing players to play until they lost, they became difficult quite quickly and allowed players to continue playing by paying a little more. This worked because of high-scores and leaderboards - people could evidently see how close their goal was, and each individual deposit was very small, so it incentivized players to keep playing.
 
-## The Rise of DLC
+All in all, I think this monetization system was fairly neutral (on a moral scale). While it did incentivize developers to make hard games, it also made them make games that anyone could pick up and play. Moreover, it motivated players to achieve mastery, as being better at the game meant you could play more for the same amount of money, so you would literally be getting more bang for your buck as you got better at playing.
 
-Downloadable content (DLC) was the first major shift away from the traditional model. Initially, DLC was genuinely additional content - expansions that added meaningful gameplay hours and new experiences.
+Nowadays, video game arcades are obsolete, as technology is powerful enough to run games on everyday accessible hardware, and there are so many free video games that even at a low admission cost it is still cheaper to play at home.
 
-However, over time, we began to see content that felt like it should have been included in the base game being sold separately. Day-one DLC became controversial, as it seemed like content was being artificially held back to generate additional revenue.
+Home Console and single-purchase
+--------------------------------
+Slowly but surely through the 80s and 90s, home consoles caught up technologically to arcade machines and became affordable enough and powerful enough to run games that people would actually consider buying.
+I will completely be skipping the video game crash of 83' here, but feel free to read about it online. I will be mainly discussing video game consoles from the moment the console market recovered.
+For the consumer, video game consoles were still a significant purchase. The console itself would cost hundreds of dollars (if not thousands, depending on the era and hardware), and each game would normally cost around 60$. Later down in history, players would also need to purchase additional controllers and memory cards in order to have the full play experience.
+All of this is to say - a lot of faith was needed from the side of consumers that they would be getting their money's worth when buying a console and/or video games.
 
-## Free-to-Play and the Microtransaction Revolution
+This proved challenging for video game developers. While home hardware has improved and could run games like never before, it still couldn't compete with arcade machines. Developers had limited memory and processing power, but were charging more money. How were they able to justify this to the player?
+Their solution was not very elegant, but it worked - they made punishingly difficult games.
 
-The introduction of free-to-play games fundamentally changed the industry. Suddenly, games could be "free" but generate revenue through small, frequent purchases.
+Consumers have come to expect from their exposure to video games via arcade cabinets that video games are hard, and that you play until you lose. This mentality was carried over to home console games, where games were even more brutal, punishing, and at times unfair with their difficulty. Players would have to sink hours of their lives mastering the controls of the game and learning its secrets and tricks in order to beat them. That way they were able to artificially lengthen the playtime of the video game, and thus make it 'worth the money' of the consumer.
+Honestly, it sounds like something that shouldn't have worked, but it did. The experience of playing games from your home, getting to progress, the invention of new video game genres, all made a novel enough experience for people to want to buy.
 
-This model has both benefits and drawbacks:
+Later down the line, as technology improved, video games actually got cheaper while their content was becoming more robust. This is still the prevalent model of selling video games for home consoles nowadays, and players are used to dishing out 20 to 80 dollars for anywhere between dozens to hundreds of hours of content.
+Also, with demos, players are now able to try out a game before buying it, making their risk much smaller.
 
-**Benefits:**
-- Lower barrier to entry for players
-- Ongoing revenue for developers
-- Ability to support games long-term
+As far as morality goes, this is in my opinion the best way to monetize a game. It's simple and straightforward as this is how most other products are sold, you get exactly what you pay for, and you have the ability to sell or trade your games in the future. Once the game is in a player's hand, game developers have no need to further market or manipulate them. They can simply focus on delivering the best player experience to them.
 
-**Drawbacks:**
-- Can become more expensive than traditional games
-- Often designed to be addictive rather than fun
-- Pay-to-win mechanics can ruin competitive balance
+DLC
+---
+A later addition to video games which became much more popular as the internet became more widespread, **downloadable content** (DLC) for existing video games which players can purchase for an additional one-time purchase became somewhat prevalent. This is a way for developers to 'expand' their existing games, normally delivering products that aren't full video games in their own right, but content that enhance the existing video game.
 
-## The Psychology of Modern Monetization
+Some DLCs are very popular, normally when they add fun and interesting content that increases playtime. Some DLCs are frowned upon, normally when they add nothing but visual enhancements to the game (Don't have time to get into it but feel free to google 'Oblivion Horse Armor').
 
-Modern monetization strategies often employ psychological techniques that can be problematic:
+Because its quality and amount of content highly varies, there is no set price for DLC. While some games deliver high-quality DLC for free, other developers sell bad DLC for a lot of money. I've seen DLC for games that cost about as much as the core game itself.
+My feelings about the morality of DLC are about the same as for other single-purchase products, if a bit lesser. I just wish that there was more standardization in its pricing model so less companies could get away with selling little to no content for a huge price.
 
-- **FOMO (Fear of Missing Out)**: Limited-time offers create artificial urgency
-- **Variable Reward Schedules**: Loot boxes use gambling-like mechanics
-- **Social Pressure**: Cosmetics create pressure to spend to fit in
-- **Sunk Cost Fallacy**: Players feel compelled to continue spending after initial investment
+Subscriptions
+-------------
+Most popular in online games, subscriptions are a service-as-a-product solution for video game monetization. The user pays a set amount of money each month and receives certain content each month. Normally, this is simply access to the game they're playing, but could also entail access to limited-time content or access to free DLCs.
 
-## Battle Passes and Live Service
+In relatively recent video game history, all console developers began their own subscription services to 'rent out' video games to their players, with the catch being they can't rent out any video game they want, and that the catalogue of games that the player has access to changes with time.
 
-The battle pass model has become increasingly popular, offering a progression system tied to both free and paid rewards. While this can provide ongoing content and goals, it also creates a "fear of missing out" if players don't complete the pass in time.
+All in all, subscription services are fairly negative in my morality book. The fact that consumers pay so much money for nothing that they truly own is pretty problematic, especially given the real-life value of accounts and items in MMOs. I think an amendment to this model that would slowly transfer ownership to consumers as their subscription goes on would be a huge improvement.
 
-Live service games promise ongoing updates and content, but often launch incomplete with the promise of future improvements.
+Free To Play
+------------
 
-## The Impact on Game Design
+Oddly enough, the free-to-play market is one of the most profitable markets in the video game industry. Some of today's most popular and profitable games, like Candy Crush, Fortnite and more, are free to play.
+Specifically on mobile platforms, apps are expected to be free. It is an expectation that's ingrained in modern culture, which I believe was inherited from the early days of the internet. While some apps do cost money, generally speaking the concept didn't stick as culturally we simply don't expect to pay for apps.
 
-These monetization strategies have fundamentally changed how games are designed:
+So how are these games profitable at all?
+Surprisingly, the answer is: in a lot of ways.
 
-- **Progression Systems**: Often artificially slowed to encourage spending
-- **Daily Engagement**: Games designed to be "sticky" rather than complete experiences
-- **Content Gating**: Core features locked behind paywalls
-- **Grinding**: Repetitive tasks designed to encourage shortcut purchases
+## Ads
 
-## Finding a Balanced Approach
+This is a concept that was inherited from webpages in the early days of the internet - if you have a page (or in this case, a game) that users come to engage with, you can show them ads while they're there. Companies will pay you to display their ads. Your content just needs to act as the middle-man.  
 
-As an indie developer, I've had to think carefully about how to monetize my games ethically. My approach has been:
+For the consumer, this is a pretty sweet deal - other than costing them some of their real-life time and attention, they don't have to pay any money in order to gain access to content that they would otherwise need to pay for.
 
-1. **Fair Pricing**: Price games appropriately for the content provided
-2. **Complete Experiences**: Ensure the base game feels complete
-3. **Optional Extras**: Any additional content should be genuinely optional
-4. **Transparency**: Be clear about what players are buying
+This is probably the most justifiable free-to-play monetization model - you're upfront about what you're asking for from the user, and it doesn't cost them anything too serious. I guess the only time it's problematic is when it hurts the play experience, but for each player the capacity for ads is different and there are enough games out there to fit anyone's criteria.
+So all in all - I'm fairly neutral on the morality of ads in games, leaning more to a positive side.
 
-## The Future of Game Monetization
+## Microtransactions
 
-The industry is still evolving. We're seeing pushback against some of the more aggressive monetization strategies, with some companies returning to more traditional models.
+Another way that games (normally free-to-play) games profit are microtransactions - small one-time purchases that players can make to get something small in return.
+This is normally combined with a bunch of clever tricks to offer these to the user when they're the most likely to engage with them. Psychologically, it's far easier for players to spend a tiny bit of money at a time, and they don't realize when they've spent a lot on multiple microtransactions. This is a strategy the video game industry has learned back in the arcade days, and serves it well today.
+Microtransactions normally fall under two categories:
 
-Subscription services like Game Pass are creating new paradigms where players pay for access to libraries rather than individual games.
+### Consumables
 
-## Conclusion
+These are in-game items or abilities that can be depleted. Depending on how the game is set up this may allow the player to do things they normally wouldn't be able to do in the game, but most often it simply makes stuff that the player can already access more widely available. It can allow them to play more often, or win more often. Stuff like that.
 
-Monetization in games isn't inherently evil - developers need to make money to continue creating games. However, the industry has sometimes lost sight of the player experience in pursuit of revenue.
+### Cosmetics
 
-The best monetization strategies are those that align developer and player interests: providing ongoing value in exchange for ongoing support.
+These are purely visual in-game changes that do not affect the balance of the game, and do not give the player any form of practical advantage. They are simply there to give the player a varied visual experience, and in the case of an online community serve as a form of self-expression and a show of wealth.
 
-As players, we vote with our wallets. Supporting ethical monetization practices and avoiding exploitative ones helps shape the industry toward better practices.
+In my opinion microtransactions are a fairly negative way to monetize a game. They are very similar to the coin-op arcade machines from back in the day, but use more psychological tricks to manipulate the player.
+Furthermore, because of the refined level of control that game developers have on the user experience nowadays, it actually incentivizes developers to make games that are unbeatable or unplayable without the use of microtransactions, to encourage players to spend more money.
 
-What are your thoughts on modern game monetization? Have you noticed changes in how you approach buying games?`,
+User Data
+---------
+
+There are heaps of websites and apps that have terms of service that show up when they're first accessed. These are written in dense legal language, and are long and boring so most users skip them and continue on to their experience.
+Unbeknownst to the average consumer, more often than not when they do this they give their consent to the developer collecting information on the user and basically doing as they please with this.
+
+While this is normally not nefarious in it of itself, the issue comes when third-party sources (not the player or the developer) gain access to the player's information. Again - this is something that the user normally agrees to in the terms of service. Unfortunately some very shady places have access to a bunch of private information because nobody wants to read the terms of service.
+
+When you see a service that is being offered completely free, and does not involve ads or micro-transactions, this is normally what it's doing. There are exceptions of course (like myself and the free games I make), but if something looks too good to be true - it probably is.
+I think this is the most morally corrupt way to monetize games, as it is fully based on tricking the user and violates their rights. Thankfully laws are catching up to technology and making it harder for developers to do this (especially in the EU), but it's still widespread.
+
+Conclusion
+----------
+
+Video games have been monetized in a plethora of ways, and I am sure as the medium will continue to evolve we will discover (or uncover) new ways to do so. These systems are not interchangeable - their effectiveness depends on the genre of game, the platform it's served on, user expectation and even the times. Still, I think that considering the monetization of games is a very helpful tool. Almost all of the time games are not made for free, so ideally those who make them would like to be paid back somehow. A game's monetization could help you understand the intentions of the developers and their attitude towards you. Also, understanding your preferences about them could inform your future experiences and purchases. Is your private information worth spending more money? Is your money less valuable than your time?
+With consideration you can be smarter about the way you engage with video games.
+
+Anyways, thanks for sticking around with me throughout this blog post. I would love to hear your opinions on the topic - if you think I misrepresented a form of monetization, if I forgot something, or anything else.
+Have a nice weekend!`,
   date: "Nov 15, 2024",
   readTime: "8 min read",
   image: blogTop10Image
