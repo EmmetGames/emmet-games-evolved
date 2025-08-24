19bc338ec9e5f2ea23f8f029b5e1d849cf19f454
Clicksurfer <amit.chameides@gmail.com>
2025-08-24 17:59:09 +1000

    Fixed formatting on the quest to connect consoles

--- src/data/posts/the-quest-to-connect-consoles.ts
+++ src/data/posts/the-quest-to-connect-consoles.ts
@@ -14,12 +14,12 @@ When you gaze up at the night sky and see a stars dotting the heavens, you are, 
 You see, the stars at which you gaze, they are not modern depictions of themselves, but rather ancient depictions that are being revealed to us in real time.
 Due to the speed of light, and due to their literally astronomical distance from us, we see the stars not as they are, but as they were ten, hundreds, thousands or more years ago. We are gazing into their past!
 It's not a still image of their past - stars are ever-changing in our skies, being created and dying. We, on Earth, right now, have a unique glimpse into how they were all those years ago.
+
 I think there is something novel about the idea of being able to capture the essence of something as it was and being able to experience it even after its time is past.
 That's probably one of the reasons I collect retro video game consoles.
 
 So for those who don't know - collecting video game consoles has been a hobby of mine for quite some time now. I have a modest collection of about 20-odd consoles, most of them being Nintendo and Sony systems. They're all in working order and I have all their cables and adapters so I can play them (though I rarely do, for reasons we'll get into later).
 I feel like it's an experience quite like no other to play an old game, holding a piece of technology and history in your hand in the form of a controller, and experiencing it like other people were 10, 20 or even 40 years ago.
-
 As a game developer, it's an honest, sobering and profound look at what video games were, how they evolved and what we can learn from them. We really don't have to invent the wheel when it was invented tens of thousands of times before us over the course of half a decade.
 Here are some pictures from my current collection, in case you were curious:
 
@@ -28,61 +28,40 @@ Here are some pictures from my current collection, in case you were curious:
 ![Image](${photo2})
 ![Image](${photo3})
 
-Where am I going with all this?
+## Where am I going with all this?
 Those among you who are astute would have noticed that none of these consoles are plugged in. In other words - they're all show (albeit a good show with the glass and lighting and whatnot).
-
 The reason might come off as a little lazy, but hooking them up is just such a pain!
 Sure, even though I keep all my cables organized and nearby, it's still a hassle of finding the right ones, then grabbing the relevant adapters (as pretty much none of these consoles use the right plug for my country, plus some of them require different voltage/current), and then moving my massive cabinet so I can fiddle with cables in the back.
 All to play what is, more often than not, a fun game, but not one I would be playing for 100 hours. More likely than not I would be playing it for 10 to 20 minutes, tops, before putting it away. As a result it often takes me longer to set up than I spend time playing the actual game.
 
-
 It's a massive shame because this collection should be put to use, and I would love to do it, just more conveniently!
-
 So this is what this blog is about (5 paragraphs in) - my quest to connect my consoles to my television so I can play them with ease!
-
 Rest assured I am still working on actual video games at this time as well, but I just figured this would be a fun project to do on the side.
 In any case, this is the perfect time since I'll be moving very soon, so if in any case I'll be moving my cabinet and reorganizing everything, why not make it better?
-
 So without further ado:
 
 
-The Goal
-I want to connect my consoles to the power and to the TV, so they will be more easily playable.
+## The Goal
+**I want to connect my consoles to the power and to the TV, so they will be more easily playable.**
 
 Obviously these are heaps of consoles, so I will have to use some sort of switch system to connect them to the TV. It doesn't have 20 ports (especially AVs).
 Same goes for the power since I don't want to be running 20 consoles at once when I am only playing one.
-
 The more convenient the solution is, the better, so I am going to give myself stretch goals - sort of side-quests, if you will, to see if I can make my solution more effective. These bonus goals are, from least to most complex
-
-Set up the display switch in such a way so I would be able to change the display remotely (via remote/app)
-
-Set up the power source in such a way so I able to turn a console on remotely (via remote/app)
-
-Somehow sync the systems together so with the press of a button/tap of an app I could turn on my TV, switch to the right display port and power the console
-
+1. **Set up the display switch in such a way so I would be able to change the display remotely (via remote/app)**
+2. **Set up the power source in such a way so I able to turn a console on remotely (via remote/app)**
+3. **Somehow sync the systems together so with the press of a button/tap of an app I could turn on my TV, switch to the right display port and power the console**
 That last one is a doozey, and I honestly doubt we'll be able to pull it off, but who knows? Maybe I'll prevail.
 
-
-The Plan
+## The Plan
 It's all well and good I want all these things, but how I am going to get this done?
-
 Well, I'm obviously going to start with research. I'll look at what others have done and the pitfalls they've hit. After that, I'll get started with the hopes of meeting the original goal somehow.
-
 Generally speaking, the plan should look like this:
-
-Research
-
-Sketch out design for solution
-
-Create holes in the back of my cabinet to allow me to pass cables to my consoles
-
-Get some electric splitters (preferably with the ability to turn individual sockets on/off) for my whole console collection
-
-Get/make a switch system for all my display cables to connect to my TV
-
-Test & tinker
-
-
+1. Research
+2. Sketch out design for solution
+3. Create holes in the back of my cabinet to allow me to pass cables to my consoles
+4. Get some electric splitters (preferably with the ability to turn individual sockets on/off) for my whole console collection
+5. Get/make a switch system for all my display cables to connect to my TV
+6. Test & tinker
 
 I guess it's time for me to start researching. I'll write another post on the blog once I have a better grasp on things.
 Thanks for reading through this, and I hope you'll check in again on my next update!`,
