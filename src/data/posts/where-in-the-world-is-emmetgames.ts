7fe8dcd77ce5c2d10a38af162d6b41d95d57fb63
Clicksurfer <amit.chameides@gmail.com>
2025-08-25 10:06:00 +1000

    Patched up where in the world is emmet games

--- src/data/posts/where-in-the-world-is-emmetgames.ts
+++ src/data/posts/where-in-the-world-is-emmetgames.ts
@@ -1,60 +1,70 @@
-import photo0 from "@/assets/the-quest-to-connect-consoles/photo0.webp";
+import photo0 from "@/assets/whereInTheWorldIsEmmetGames.avif";
 import { BlogPost } from "../blogPosts";
 
 export const whereInTheWorldIsEmmetgames: BlogPost = {
   id: "where-in-the-world-is-emmetgames",
   title: "Where in the World is EmmetGames?",
   excerpt: "An update on recent travels, new inspirations, and how experiencing different cultures influences game design.",
-  content: `# Where in the World is EmmetGames?
+  content: `Hey everyone! Hope you're doing well.
+You may or may not have noticed, but it's been a hot minute since I've posted on this blog. In fact, it has been the longest break I've taken since I published this website.
 
-I've been pretty quiet on the blog lately, and there's a good reason for that - I've been traveling! Not for business, just for the pure joy of experiencing new places and cultures.
+I'd love to tell you all what I've been doing, and why I haven't posted in so long. Let's start with a small recap of what I've been up to:
 
-## The Journey
+*   Gotten married
+*   Moved back to Australia 
+*   Worked on and released some Game Jam entries 
+*   Hosted my own Game Jam 
+*   Started (and dropped) development on multiple games 
+*   Started (and completed) development on some games which I can't really share here
+*   Kept working at Beach Bum Ltd
 
-Over the past few months, I've had the opportunity to visit several countries, each offering unique perspectives on design, storytelling, and player experiences.
+Yea, it most certainly has been a busy break.
 
-### Japan: The Land of Game Design
+**What am I up to nowadays?**
 
-My time in Japan was particularly inspiring from a game development perspective. Visiting arcades in Akihabara, seeing how people interact with games in public spaces, and experiencing the attention to detail in everything from train station signage to vending machine interfaces.
+There are two main things I'm cooking up right now.
 
-The concept of "omotenashi" (hospitality) really struck me - this idea of anticipating needs before they're expressed. It's something I want to bring to my game design: creating experiences that feel intuitive and welcoming without being obvious about it.
+The first, unsurprisingly, is my work in Beach Bum. I've recently been promoted to architect there, so along with my existing responsibilities as team lead it's been really busy. In general, working Israeli hours remotely from Australia is really tough. Thankfully I'm a night person, but I'm still a person and it's impossible to work these hours without some level of social isolation.
+I'm thankful I have a supporting wife and that my work is as accommodating as possible for this. It wouldn't have been possible otherwise. As for what exactly I'm working on in the company - as I'm signed on an NDA I can't share anything specific, but the general gist is that I'm making new stuff, and it's very fulfilling.
+Over the years in the company I've gotten to work on all the games it has made (including the non-released ones), and by far one of the most rewarding things about my job is getting to work with the different dev teams and learn about their game, their dynamic, and their code.
+You'd be amazed how different teams operate completely differently, even in the same company. Getting to learn from all of them is a privilege.
 
-### Northern Europe: Minimalism and Function
+The second is a PC game. At the moment, I don't know if I will release it when I finish it. It's a digital recreation of a board game a good friend of mine designed. It's not pretty, but it's functional. The main point of it is to be able to playtest the game far more easily than with the physical copy.
+All I can say is that I'm happy with my code there. I've utilized Unisave (which I've discussed in my previous blog post) to create all the server-side for this multiplayer game, and Unisave doesn't disappoint. It does have its quirks, which I may get into at some point. We'll see.
 
-Time spent in Scandinavia exposed me to a different design philosophy - one focused on minimalism and function over decoration. The way Nordic designers approach user interfaces and experience design has me rethinking some of my own aesthetic choices.
+**Why haven't I been posting?**
 
-There's something beautiful about removing everything that isn't essential and making what remains work perfectly.
+As I see it, there are two major contributors to this.
 
-## How Travel Influences Design
+The first is an excuse as old as time - I've been busy.
 
-Travel changes how you think about design in several ways:
+Basically following up from my last post, I've had a more or less finished version of Pyramid Scheme (on a technical level). Visually it was very lacking, which was annoying because the only way for it to make it would be for the game to be visually appealing.
+What followed was numerous failed attempts by me to collaborate with artists to make UI for this game. Who knew that people wouldn't be psyched to work on art in their free time for a game that will make no money?
+I can't say that I'm not entirely disappointed, but it's what happened. Perhaps I have a ways to go when it comes to indie collaboration.
 
-**Perspective on Universal vs. Cultural Design**: Some design principles are universal, while others are deeply cultural. Understanding this helps create games that can appeal to global audiences while still having unique character.
+Following this, and sorry for the abrupt transition, my friend died. I won't get into the specifics because it doesn't feel right to, but this sucked pretty bad and affected me mentally for many months. They were an awesome person and I felt like I needed to do something to commemorate them in a way they would want to be remembered. I came up with the idea of making a game jam in their honor - and that's what I did. This basically consumed a half-year of my life as I studied and participated in game jams in preparation, created the game jam, created marketing for the event, promotional videos, social media presence, etc.
+The game jam was _fine_. I'm really grateful to those who participated in it, but turnup was smaller than I anticipated, and I wasn't able to raise any money for the charity I was partnered up with.
+It sucks knowing I could've sunk the advertising costs into donations to the charity and possibly have done more good in the world, not to mention saving me a stressful half-year.
+I wish I had the retrospective knowledge to know exactly where I screwed up with the marketing of this event and its design, but I don't know.
+I'm glad I made that game jam, but it wasn't necessarily good for my mental health.
+I'm purposefully not sharing the name of the game jam here nor any links to it because this page is about my game dev efforts, and while this is related to making games, this game jam was all about my friend - not me.
 
-**Problem-Solving Approaches**: Different cultures approach problems differently. Seeing these varied approaches gives you more tools in your design toolkit.
+After this, I got swept up in planning my wedding and moving to Australia. Re-settling in here was very time and effort consuming. So it was only a few months ago that I found myself able to work on games again in my free time.
 
-**Aesthetic Influences**: Color palettes, architectural forms, natural environments - all of these can inspire visual design choices.
+The second contributor to why I haven't been posting is the philosophy of posting itself.
 
-**Storytelling Traditions**: Every culture has its own storytelling traditions and tropes. Understanding these can help create more authentic and diverse narratives.
+I know that basically nobody reads these posts. In general, people don't even like to read - they prefer video. It feels insane to explain, but it's not naturally intuitive for human beings to open a webcam, start describing what stuff they've been up to, and then editing that video for upload. Like, I'm a game dev. I make games. That's my passion - that's what I love. I like to share my games with others, and I like to tell others about what I'm up to, but it's not easy or intuitive to do so in a 'fun', 'digestible' way.
+Making vlog posts was always a bit of a chore for me. I did it because I felt like it was the only way for people to engage and listen to me. I was trying to make indie development my career, and I was desperately trying any trick I could think of to make it happen.
 
-## Bringing It Back
+For better or worse, this isn't the case anymore. I have been working full-time in a game studio for a couple of years now. My urge to develop games in my free time has gotten smaller. Not disappeared - I've still done it multiple times, but it's certainly not as strong as it was in the past. It's a shame because I love what I make on my own - it's my self-expression, me proving to myself that I have the capacity to create and be original, but I think it's also a good thing. Not 'needing' to make games on the side means that to some capacity I'm happy where I am and what I'm doing in my day-to-day.
 
-I'm excited to apply some of these insights to my next project. I'm planning a game that draws inspiration from several of the places I've visited, trying to capture not just the visual aesthetics but also the underlying design philosophies.
+I am, and always will be, an indie developer. The only two things that will change are how much time I set aside for it and how much I will depend on it for my living wage. I am at a point in my life where I don't have to depend on my indie development for money, and I do have some time and patience to dedicate to it every week to keep making games. I'm happy that way.
+So for now on, unless I feel the need to change it up in the future, I'll return to working in blog posts rather than vlogs. Running with the theme of my indie development - I am creating weird things that are fun for me to create, and writing these summaries of what I'm up to and what's on my mind is fun for me.
 
-## The Value of Stepping Away
+If you're reading this, first of all - wow. That's a lot to read. But also - thank you! I'm glad my ramblings are of any interest to you. I hope you stick around as I continue to log my development.
 
-Sometimes the best thing you can do for your creative work is to step away from it entirely. This trip reminded me why I love making games in the first place - they're a way to create experiences and share perspectives with people around the world.
-
-Coming back to development with fresh eyes and new inspiration feels incredible.
-
-## What's Next?
-
-I'm back home and diving into pre-production on the next project. Expect to see some travel influences creeping into the design, but more on that in future posts.
-
-For now, I'm just grateful for the opportunity to explore the world and bring those experiences back to my work.
-
-Have you ever had travel experiences that influenced your creative work? I'd love to hear about them!`,
-  date: "Sep 18, 2024",
-  readTime: "4 min read",
+Anyways, that's it for now. Thanks for checking in, and have a good one.`,
+  date: "Jan 31, 2024",
+  readTime: "5 min read",
   image: photo0
 };
