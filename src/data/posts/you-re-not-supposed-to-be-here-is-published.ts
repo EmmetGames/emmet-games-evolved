85269b7d99d315cb304d2bcacdfcc10c91e00b01
Clicksurfer <amit.chameides@gmail.com>
2025-12-01 14:28:00 +1100

    Added another webpage

---
+++ src/data/posts/you-re-not-supposed-to-be-here-is-published.ts
@@ -0,0 +1,26 @@
+import { BlogPost } from "../blogPosts";
+import image from "@/assets/youre_not_supposed_to_be_here_app_icon.png";
+
+export const youreNotSupposedToBeHereIsPublished: BlogPost = {
+  id: "you-re-not-supposed-to-be-here-is-published",
+  title: "You're Not Supposed To Be Here is published!",
+  excerpt: "After months of work, my game is finally out on the play store.",
+  content: `After months of work, my game is finally out on the play store. Let me just say that publishing takes time and you have to play around with keys and it's definitely not a perfect system at the moment. Beware, future developers!
+
+![image](${image})
+
+https://play.google.com/store/apps/details?id=emmetgames.game.youre\_not\_supposed\_to\_be\_here  
+
+Overall, I feel pretty bittersweet about this game. I met all my original goals (As visible in the earliest posts related to this topic), and made a game that's really worth playing.
+
+However, it is also a relatively buggy game, which may still be hard for new players to grasp, even with a tutorial. It could've been something far far greater, and it was just 'nice', and this is due to my skills as a developer.
+
+But hey, this is exactly how I improve, so I'm on the right path. On to the next game!
+
+In any case, please do download and play the game. I'll make a new Google Form for feedback in the upcoming days.
+
+Catch ya later!`,
+  date: "Aug 12, 2019",
+  readTime: "1 min read",
+  image: image
+};
