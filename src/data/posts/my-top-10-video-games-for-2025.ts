992ec630964c61e2d7f9fac36fa38f40cca9c177
Clicksurfer <amit.chameides@gmail.com>
2025-12-19 22:14:26 +1100

    Started writing top 10 again

---
+++ src/data/posts/my-top-10-video-games-for-2025.ts
@@ -0,0 +1,135 @@
+import blogTop10Image from "@/assets/blog-top10-2025.webp";
+import game1 from "@/assets/top-ten-2025/TopTenGames1.png";
+import game2 from "@/assets/top-ten-2025/TopTenGames2.png";
+import game3 from "@/assets/top-ten-2025/TopTenGames3.png";
+import game4 from "@/assets/top-ten-2025/TopTenGames4.png";
+import game5 from "@/assets/top-ten-2025/TopTenGames5.png";
+import game6 from "@/assets/top-ten-2025/TopTenGames6.webp";
+import game7 from "@/assets/top-ten-2025/TopTenGames7.png";
+import game8 from "@/assets/top-ten-2025/TopTenGames8.png";
+import game9 from "@/assets/top-ten-2025/TopTenGames9.png";
+import game10 from "@/assets/top-ten-2025/TopTenGames10.png";
+
+import { BlogPost } from "../blogPosts";
+
+export const myTop10VideoGamesFor2025: BlogPost = {
+  id: "my-top-10-video-games-for-2025",
+  title: "My Top 10 Video Games for 2025",
+  excerpt: "",
+  content: `## Introduction
+
+2025 has been a very busy & interesting year for me on a personal level, but none of that matters now. We're here to talk about my top 10 games for the year.
+Some picks here are no-brainers, and some picks would baffle most, I imagine. Take it with a grain of salt - I play a lot of games and make them too, but I have specific tastes.
+
+I will note that I've played about 50 games this year, and liked over 20 of them enough to think about if they would make my top 10 list. Some of the games that didn't make the cut I've played for hundreds of hours.
+All of this is to say, I truly love all of the top 10 games on this list and would highly encourage you to check 'em out if they're up your alley!
+
+## Number 10 - RimWorld
+
+![image](${game10})
+
+Earlier this year I played _Dwarf Fortress_ for the first time. While I adore the lore behind its development, and respect the game for its technical ingenuity, I do also think it's not a very fun game.
+It looks unimpressive and controls poorly, and so information-dense that it's hard to tell the forest from the trees.
+
+I bring all this up because _RimWorld_ feels like the natural evolution of that game - a modern masterpiece which took all the elements that made _Dwarf Fortress_ great & imporved on every problematic aspect.
+
+In this game you control a group of space colonists as they try to survive on a faraway planet, inhabited by all sorts of folk and wilderness that can be unkind.
+It's a management sim game where you really get to shape your colony and create different types of communities.
+Some of the game's best aspects stem from its generated narrative - each character you encounter, as well as your own colonists, are generated with histories and stats and quirks. They interact with each other in novel ways and lead to emergent gameplay.
+
+_Ludeon Studios_, an indie studio founded by Tynan Sylvester in 2023 did an amazing job with this game and has attracted a cult following since.
+I'm highly impressed with this game and while I've sunk dozens of hours into it, I feel like I could easily sink hundreds more. A slippery slope of a game.
+
+## Number 9 - Plants Vs. Zombies
+![Complete cabinet](${game9})
+
+Lo and behold - while I've played the original _Plants Vs. Zombies_ on my friends' iPod Touchs growing up, I never really got to play it in full. By the time I had my own smartphone the game was virtually inaccessible.
+However, to this day, the game is available on Steam (though I will say, this PC port is not super clean). This year I figured I'd shill a few bucks and give it a whirl - and I'm extremely glad I did.
+
+This game is just as tight as it was over a decade ago - the levels are fun, the different plants you constantly unlock totally change up the gameplay, and all in all it just delivers a seriously solid experience.
+With the added challenge modes, and Ned's shop, there's plenty to unlocks and do even after beating the main campaign.
+
+It truly boggles the mind how a game as popular as _Plants Vs. Zombies_, which reimagined the tower defense genre in such a brilliant way, did not have a billion knockoff games.
+Considering it's incredibly low pricepoint, I highly recommend playing through this game if you haven't already. It's a timeless classic.
+
+I'll of course shout-out its developer George Fan from _PopCap_ at the time, who also developed the great _Insaniquarium_, among other great things.
+
+## Number 8 - FTL Multiverse
+![image](${game8})
+
+One of the best parts of owning a website with my name on it is that I get to make the rules.
+This is why my number 8 game of 2025 is a MOD for the game _FTL (Faster Than Light)_ by _Subset Games_.
+
+_Multiverse_ is an insanely impressive free-to-download mod developed by a bunch of passionate fans for the original _FTL_ game.
+For those who don't know, _FTL_ is a game where you manage a spaceship as you fly between encounters and try to escape the rebel fleet. It's a great game with roguelite elements that has a lot of unique mechanics.
+These folks have worked tirelessly for a while to find ways to tweak the source code of the game and modify virtually every aspect of it.
+The result of their herculean effort is what feels like the sequel to _FTL_:
+
+Dozens of new, different spaceships, heaps of new species, new weapons, questlines, mechanics, bosses. It trully boggles the mind how good all of it is and how much of it there is.
+I've played _Multiverse_ for probably longer than I've played the original game, and I'm not even close to scraping the surface of all the content it has to offer.
+
+I must say that in all of my years gaming, I've never been so impressed with a mod. It's an absolute achievement and a testamet to the power of persistent and passionate people.
+
+The only thing I have to note is - because it expands on the original _FTL_ so much, I would say that if you haven't played the classic yet you should play it without _Multiverse_ first.
+
+## Number 7 - Tavern Keeper
+
+It feels a little bad to put a game on this list that's currently in Early Acccess, but I've been waiting for this game too long NOT to play its first available version.
+At time of writing about half of its content is available.
+Thankfully, this content is hella fun to play.
+
+In _Tavern Keeper_, you play the owner of a fantasy tavern, and get to manage it in various fantasy settings.
+It's a very humurous game with a solid management aspect to it.
+One of its main features is the ability to basically make your own furniture out of many parts, sort of like the _Spore_ creator.
+To be honest, I didn't care much for this aspect of the game, but others seemed to really love it!
+
+I think it's impressive in it of itself that the game has so much to offer that even with a part of the game not appealing to me, I still had a blast playing it.
+I've played lots of management games over the years and it's so nice to see how optimized this game is - that is to say, how it learned from everybody's mistakes and made a really polished and fun management game that's equal parts challenging & creative.
+The team making it, _Greenheart Games_, have been working on it tirelessly ever since they wrapped up with _Game Dev Tycoon_ all these years back.
+I've had the chance to meet them in the past and they're super lovely.
+
+All in all, I reckon _Tavern Keeper_ is one of the best management games to come out in recent years and I highly anticipate its full release.
+
+## Number 7 - Dredge
+
+![image](${game7})
+
+_Dredge_ is one of those amazing 
+
+## Number 6 - The Forgotten City
+
+![image](${game6})
+
+
+
+## Number 5 - Reus 2
+![image](${game5})
+
+
+
+## Number 4 - Disco Elysium - The Final Cut
+![image](${game4})
+
+
+
+## Number 3 - Baldur's Gate 3
+![image](${game3})
+
+
+
+## Number 2 - We Love Katamari REROLL + Royal Reverie
+![image](${game2})
+
+
+
+## Number 1 - Animal Well
+![image](${game1})
+
+
+
+`,
+  date: "Dec 31, 2025",
+  readTime: "X min read",
+  image: blogTop10Image,
+  featured: true
+};
