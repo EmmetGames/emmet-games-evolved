6b6e3d567b948c92624f0ba2433d28f968e31fa7
Clicksurfer <amit.chameides@gmail.com>
2026-02-23 15:58:42 +1100

    Added patches

--- src/data/posts/the-contradictions-of-persona-5.ts
+++ src/data/posts/the-contradictions-of-persona-5.ts
@@ -16,11 +16,11 @@ I’ve recently finished playing *Persona 5* for the first time, and I have some
 
 ![image](${image})
 
-In short, Persona 5 is a massively successful JRPG, a spin-off game which has its own spin-offs.  
+In short, *Persona 5* is a massively successful JRPG developed by Atlus, a spin-off game which has its own spin-offs.  
 I’ve been playing the original version on PS3. I tried playing it years ago only to be deterred by the first couple of hours being just people saying mean stuff to you, but this year I decided to persevere and try again, and I’m glad I did.  
 I stayed away from discourse surrounding this game, so my opinion is purely my own and untainted by the public. I honestly have no idea which of my opinions and ideas are popular or unpopular.
 
-Basically I think Persona 5 is a good game with some serious problems. More than anything, however, it is a curious case of conflicting design choices and contradictions, and I’d like to explore that.
+Basically I think *Persona 5* is a good game with some serious problems. More than anything, however, it is a curious case of conflicting design choices and contradictions, and I’d like to explore that.
 
 What do I mean by contradictions and design choices?  
 You'll soon see.
@@ -30,9 +30,9 @@ I think a lot of what I write is going to come off as negative, so I reckon it�
 
 ![diner](${diner})
 
-On an audio-visual level, Persona 5 is almost unparalleled. Its UI in particular is fantastic \- every menu, every element is stylish, legible and full of personality. The soundtrack is an absolute bop and it’s great how many iconic tracks it has while keeping to the general motifs and genre of the game.
+On an audio-visual level, *Persona 5* is almost unparalleled. Its UI in particular is fantastic \- every menu, every element is stylish, legible and full of personality. The soundtrack is an absolute bop and it’s great how many iconic tracks it has while keeping to the general motifs and genre of the game.
 
-I think the characters are well-written and interesting. They’re all complex and their personal journeys are interesting to follow. Partially because of the great writing for the characters, partially because of the surprisingly good voice-acting, and partially because of just how long you spend with these characters, you really get attached to them.  
+I think the characters are well-written and interesting. They’re all complex and their personal journeys are interesting to follow. This is partially because of the great writing for the characters, partially because of the surprisingly good voice-acting, and partially because of just how long you spend with these characters. The end result is that you really get attached to them.  
 When romance eventually becomes a thing in the game, it feels super believable because it feels earned, like there is a real underlying history, relationship and chemistry.
 
 I think the gameplay, while simplistic, is fun. Because it’s a long and repetitive game, it’s a pretty great wind-down game when you’re looking for an hour to kill. Because you’re constantly getting stuff done in the game, it’s great for scratching that productivity itch.  
@@ -50,11 +50,11 @@ Now let’s get to the more bizarre, frustrating and weird design choices of the
 
 The combat system is fairly standard for JRPGs \- you got party members, physical attacks and magic attacks (AKA elemental Persona attacks), items and the ability to block.  
 You’d think that the gameplay would look like other JRPGs too, right? Whittle down the enemy’s health, with each party member having a distinct class, right?  
-Wrong. Persona 5’s combat is all about finding & exploiting enemy weaknesses. Battles are decided on the first turn mostly, as you use an attack the enemy’s weak to and then finish them off while they’re down.  
+Wrong. *Persona 5*’s combat is all about finding & exploiting enemy weaknesses. Battles are decided on the first turn mostly, as you use an attack the enemy’s weak to and then finish them off while they’re down.  
 It creates a bizarre state of affairs where your MP is the main deciding factor for whether you’ll win or lose a fight, or how long you can go exploring for. HP and its recovery are trivial, and weapon upgrades are rarely necessary.
 
 That’s all well and good, but where’s the contradiction?  
-Well, in most boss fights the enemy does not really have an exploitable weakness, and there you do go back to the standard JRPG affair. It’s just really odd for boss fights, which should often be a test of the skills you’ve gained playing the game, to effectively bring in a different rulebook altogether.
+Well, in most boss fights in *Persona 5* the enemy does not really have an exploitable weakness, which means you go back to the standard JRPG affair. It’s just really odd that for boss fights, which should often be a test of the skills you’ve gained playing the game, the gameplay changes in such a dramatic way.
 
 # Relationships and bonds
 
@@ -62,7 +62,7 @@ Well, in most boss fights the enemy does not really have an exploitable weakness
 
 A huge part of the game revolves around creating & deepening bonds with various people throughout Tokyo, but predominantly your schoolfriends. I would even go as far to say that it’s the secondary objective of the entire game, short of the actual win/lose objective.  
 Throughout your playthrough you’re constantly told about the importance of deepening these bonds, and it really feels like every other mechanic circles back to that.  
-That’s why it’s so jarring that you don’t have any real gameplay motivation to continue seeing characters after maxing-out your relationship with them. That’s crazy \- if anything, you would expect to be spending the most time with your closest comrades and friends, but you actually don’t have a good reason to do so. If anything, in order to play the game ‘right’ and get as much stuff as possible, you’re supposed to stop spending time with them altogether.  
+That’s why it’s so jarring that you don’t have any real gameplay motivation to continue seeing characters after maxing-out your relationship with them. That’s crazy. If anything, you would expect to be spending the most time with your closest comrades and friends, but you actually don’t have a good reason to do so. In reality the opposite is true - in order to play the game ‘right’ and get as much stuff as possible, you’re supposed to stop spending time with them altogether.  
 This is an immersion-breaking, stupid design choice, and it’s only worsened when it comes to dating characters.  
 By mechanically incentivicing you to stop seeing a character after you’ve started dating them because you no longer gain a benefit from doing so, you’re cheapening the relationship and sending a horrible message about romantic partners being trophies you earn rather than two-way relationships that you maintain.  
 It’s such a shame too because the game was very good in establishing the bonds of the characters leading up to romance \- it really felt like it could convey relationships in meaningful, mature ways.
@@ -71,16 +71,16 @@ It’s such a shame too because the game was very good in establishing the bonds
 
 ![day system](${daySystem})
 
-The story of Persona 5 takes place over several days, which are mostly divided into a ‘day’ activity and a ‘night’ activity. These activities basically boil down to deepening your connections to NPCs, which can give you all sorts of buffs, as well as a story about that character, or boosting stats which may be required to continue meeting some NPCs.  
+The story of *Persona 5* takes place over several days, which are mostly divided into a ‘day’ activity and a ‘night’ activity. These activities basically boil down to deepening your connections to NPCs, which can give you all sorts of buffs, as well as a story about that character, or boosting stats which may be required to continue meeting some NPCs.  
 During normal play, you have a limited number of days to complete the current dungeon, so you’re always in a race against the clock trying to find ways to get the most bang for your buck in the time you have.  
 Exploring the dungeons takes a lot out of your party \- you have to start in the daytime and it consumes both ‘day’ and ‘night’ for that day.  
-This means that, on a gameplay level, you’re incentivized to try and get as much done in a dungeon visit. For skilled players, this means starting and finishing the dungeon in 1 day (or the minimum possible amount of days). This is jarring to begin with because in terms of gameplay it’s not super fun, and clearly in terms of narrative that’s not the intention.
+This means that, on a gameplay level, you’re incentivized to try and get as much done in a dungeon visit. For skilled players, this means starting and finishing the dungeon in 1 day (or the minimum possible amount of days). This is jarring because in terms of gameplay it’s not super fun, and also it's clearly not the intention in terms of narrative.
 
 “Take Your Time”. That’s the text that appears on every loading screen in the game. The narrative really seems to emphasize that you’re a part-time student, part-time vigilante. It’s clear the intention of the game from a narrative standpoint is that you juggle day-to-day activities with progressing through dungeons.  
 You’re given heaps of different activities to do \- too much to do in one playthrough, frankly \- and you have no real way of learning what things do without interacting with them at least once.  
 In short \- it’s a slice of life crossed with a dungeon-exploring RPG.
 
-What ended up happening to me is that I ended up beating dungeons in one go, and then spent several days doing the slice of life activities \- except there I would talk to people or grind stats in order to be able to continue storylines.  
+What ended up happening for me is that I ended up beating dungeons in one go, and then spent several days doing the slice of life activities \- except there I would talk to people or grind stats in order to be able to continue storylines.  
 It’s a shame because clearly the game wants to be immersive, but it feels anything but when you beat a dungeon that’s supposed to take weeks in one day, and then proceed to spend 2 weeks eating burgers nonstop to boost your grit stat.
 
 What’s worse is that since this is a time-limited game, you will straight out be locked out of content \- despite telling you that you have time, realistically you will get to a point in the game where you won’t get to deepen your bonds with people anymore. Some of the activities that are out there are flat-out sub-optimal choices, and engaging with them will subsequently lock you out of end-game content, which is nuts.  
@@ -101,7 +101,7 @@ So it made me wonder \- why did it feel so different when it came to this game?
 I can theorize a couple of factors which might have made it feel different in this game.  
 
 ## The Butterfly Effect
-At the end of the day, you will not be locked out of content in Persona 5 because you picked a button on the endingtron 3000 in a game like Mass Effect or Deus Ex.  
+At the end of the day, you will not be locked out of content in *Persona 5* because you picked a button on the endingtron 3000 in a game like Mass Effect or Deus Ex.  
 You will be locked out of content because of the activities you incrementally chose to do over the course of the game \- a death by a thousand cuts.  
 You can’t just ‘save before the choice’ and then see what happens otherwise.  
 Unless you use an online guide, a calculator using scraped game data, or are replaying the game from the start, it would be virtually impossible to prevent at least some content from locking out.
@@ -115,29 +115,29 @@ This gives a much more meaningful ‘finality’ to the outcome, as it’s a ser
 You’re eventually locked out of some activities, some personas, and some personal storylines of some NPCs.  
 Not only are these not really necessary to complete the game, which isn’t too hard, but also they don’t even prevent you from getting the ‘true ending’. In other words, it’s insignificant enough that the developers likely thought it was fine.  
 The thing is, in a long game where you have tedious combat and same-y activities, it’s the storyline and character-to-character interaction that’s the most interesting \- and rewarding \- to see.  
-It’s not a traditional gameplay reward, but personally I’d say it’s one of the most valuable things in Persona 5\.
+It’s not a traditional gameplay reward, but personally I’d say it’s one of the most valuable things in *Persona 5*\.
 
 All these factors led to a deep feeling of dissatisfaction when I could no longer talk to some characters or do some things, and yet?  
 In some ways, it felt surprisingly mature and introspective.
 
 ## Reflection
 The game is all about putting you in the shoes of another character as they go through a year of their life. They make choices, and they don’t get to experience everything \- they deal with the consequences of their actions.  
-This is true for us humans as well. When we sit down to play a game, we’re doing that instead of literally anything else. That is time we will never get back, and much like the protagonist of Persona 5, we have a limited amount of days to do what it is we want to do.  
+This is true for us humans as well. When we sit down to play a game, we’re doing that instead of literally anything else. That is time we will never get back, and much like the protagonist of *Persona 5*, we have a limited amount of days to do what it is we want to do.  
 Opportunities will disappear, and we will be locked out of certain choices and relationships.  
 Much like the protagonist, we make incremental choices day-to-day that have massive ramifications down the line in our life.  
 In some way, this game reflects the human condition in a profoundly meaningful way.  
 I don’t know if this was a message that Atlus had in mind when developing this game, but regardless \- by blocking off content in such a strong and radical way, it was able to convey this message powerfully.
 
 I would be lying if I said that it didn’t matter to me if the game designers had this in mind or not. I think it would cheapen the impact of this message if it was accidental. Us humans can find meaning in just about anything, but that doesn’t mean that it’s worth listening to.  
-In any case, moving forward, I will keep Persona 5 in mind and try to reflect on both its successes and shortcomings in order to shape how I make games.  
+In any case, moving forward, I will keep *Persona 5* in mind and try to reflect on both its successes and shortcomings in order to shape how I make games.  
 In an ironic twist of fate, by incrementally playing this game to completion I will cause an effect on the future games I make.
 
 # Conclusion
 
 ![conclusion](${conclusion})
 
-Thanks for joining me on the ramblomatic mess. I think that whenever a game makes you feel anything it’s worth examining, because evoking feelings is one of the most powerful things a game can do.  
-I’ve learned from it and enjoyed it, probably more than some and almost certainly less than others.  
+Thanks for joining me on the ramblomatic mess. I think that whenever a game makes you feel anything - it’s worth examining. Evoking feelings is one of the most powerful things a game can do.  
+I’ve learned from *Persona 5* and enjoyed it, probably more than some and almost certainly less than others.  
 Feel free to reach out to me with your own thoughts and feelings about this game. I’d love to expand my views on it. Thank you.
   `,
   date: "Feb 23 2026",
